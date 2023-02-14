2f4e4d1b23856d3d6a2778fa99d0f45f2c0dcb87
Abdur Razzak Romy <41229666+Razzakromy@users.noreply.github.com>
2023-02-14 20:56:22 +0600

    updated

--- script.js
+++ script.js
@@ -1,10 +1,23 @@
-    <!---------------js----------------------->
-    <script src="https://code.jquery.com/jquery-3.6.3.js" integrity="sha256-nQLuAZGRRcILA+6dMBOvcRh5Pe310sBpanc6+QBmyVM=" crossorigin="anonymous"></script>
-    <script src="https://cdnjs.cloudflare.com/ajax/libs/lightslider/1.1.6/js/lightslider.min.js"></script>
-    
-   
-    <script> 
-    $(document).ready(function() {
+// Navbar
+(function($) {
+
+	"use strict";
+
+	$('nav .dropdown').hover(function(){
+		var $this = $(this);
+		$this.addClass('show');
+		$this.find('> a').attr('aria-expanded', true);
+		$this.find('.dropdown-menu').addClass('show');
+	}, function(){
+		var $this = $(this);
+			$this.removeClass('show');
+			$this.find('> a').attr('aria-expanded', false);
+			$this.find('.dropdown-menu').removeClass('show');
+	});
+
+})(jQuery);
+
+$(document).ready(function() {
         $('#autoWidth').lightSlider({
             autoWidth:true,
             loop:true,
@@ -13,5 +26,3 @@
                 } 
         });  
     });
-        
-    </script>
