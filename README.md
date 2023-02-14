5114eac6d68a164c3d8108025932ffcb1a922251
Abdur Razzak Romy <41229666+Razzakromy@users.noreply.github.com>
2023-02-14 16:53:54 +0600

    Initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# e-com
+ for ayons mam
---
+++ script.js
@@ -0,0 +1,17 @@
+    <!---------------js----------------------->
+    <script src="https://code.jquery.com/jquery-3.6.3.js" integrity="sha256-nQLuAZGRRcILA+6dMBOvcRh5Pe310sBpanc6+QBmyVM=" crossorigin="anonymous"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/lightslider/1.1.6/js/lightslider.min.js"></script>
+    
+   
+    <script> 
+    $(document).ready(function() {
+        $('#autoWidth').lightSlider({
+            autoWidth:true,
+            loop:true,
+            onSliderLoad: function() {
+                $('#autoWidth').removeClass('cS-hidden');
+                } 
+        });  
+    });
+        
+    </script>
